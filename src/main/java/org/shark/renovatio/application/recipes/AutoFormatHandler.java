ce234978c48163bb7583c856b00a74fcc6ff59db
FabianAG <faguero@gmail.com>
2025-09-11 14:00:56 -0300

    Refactor service to delegate recipe handling

--- src/main/java/org/shark/renovatio/application/recipes/AutoFormatHandler.java
+++ src/main/java/org/shark/renovatio/application/recipes/AutoFormatHandler.java
@@ -2,6 +2,12 @@ package org.shark.renovatio.application.recipes;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * Simple handler that normalizes whitespace and line breaks to mimic the
+ * behaviour of OpenRewrite's {@code AutoFormat} recipe. The component name is
+ * set to the fully qualified recipe identifier so that it can be looked up in
+ * the handler map.
+ */
 @Component("org.openrewrite.java.format.AutoFormat")
 public class AutoFormatHandler implements RecipeHandler {
     @Override
