37bcb2e5088d193d751d656a127b6caac9d841ad
faguero <faguero@gmail.com>
2025-09-24 21:17:51 -0300

    refactor(pom): reorder modules for improved structure and clarity

--- renovatio-client/src/main/java/org/shark/renovatio/client/TestClass.java
+++
@@ -1,8 +0,0 @@
-package org.shark.renovatio.client;
-
-public class TestClass {
-    public static void main(String[] args) {
-        System.out.println("TestClass ejecutado correctamente.");
-    }
-}
-
